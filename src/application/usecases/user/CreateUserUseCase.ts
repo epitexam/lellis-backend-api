eb4596bd135b9ae440ea42cc0e800c34c5e66317
Epitexam <202238868+epitexam@users.noreply.github.com>
2025-12-01 02:08:47 +0100

    Improving create user use case

--- src/application/usecases/user/CreateUserUseCase.ts
+++ src/application/usecases/user/CreateUserUseCase.ts
@@ -1,29 +1,43 @@
 import { CreateUserDTO } from "../../../domain/user/dtos/CreateUserDTO";
 import { User } from "../../../domain/user/entity/User";
-import { UserError, UserErrorType } from "../../../domain/user/enums/UserErrorType";
 import { Email } from "../../../domain/user/valueObjects/Email";
 import { Password } from "../../../domain/user/valueObjects/Password";
 import { IPasswordHasher } from "../../providers/IPasswordHasher";
 import { IUserRepository } from "../../repositories/user/IUserRepository";
+import { UserError, UserErrorType } from "../../../domain/user/enums/UserErrorType";
+import { IIdProvider } from "../../providers/IIdProvider";
 
 export class CreateUserUseCase {
+    /**
+     * @param {IUserRepository} userRepository - Persistence abstraction
+     * @param {IPasswordHasher} passwordHasher - Password hashing abstraction
+     * @param {IIdProvider} idProvider - ID generation abstraction (not hardcoded)
+     */
     constructor(
         private readonly userRepository: IUserRepository,
         private readonly passwordHasher: IPasswordHasher,
+        private readonly idProvider: IIdProvider,
     ) { }
 
+    /**
+     * Executes the user creation process.
+     *
+     * @param {CreateUserDTO} input - Data transfer object containing user creation data
+     * @returns {Promise<User>} The created user entity (with hashed password)
+     * @throws {UserError} if email is already used or validation fails
+     */
     async execute(input: CreateUserDTO): Promise<User> {
         const email = Email.create(input.email);
-
-        const existing = await this.userRepository.findByEmail(email);
-
-        if (!existing) throw new UserError(UserErrorType.EMAIL_ALREADY_USED);
-
         const password = await Password.create(input.password, this.passwordHasher);
 
+        const existingUser = await this.userRepository.findByEmail(email);
+        if (existingUser) {
+            throw new UserError(UserErrorType.EMAIL_ALREADY_USED);
+        }
+
         const user = User.create({
-            id: Bun.randomUUIDv7(),
-            email: Email.create(input.email),
+            id: this.idProvider.generate(),
+            email,
             password,
             firstName: input.firstName.trim(),
             lastName: input.lastName.trim(),
@@ -31,6 +45,6 @@ export class CreateUserUseCase {
 
         await this.userRepository.save(user);
 
-        return user
+        return user;
     }
 }
