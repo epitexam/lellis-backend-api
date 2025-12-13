120304147d11a72c6641df4d67500bd10c685333
Epitexam <202238868+epitexam@users.noreply.github.com>
2025-12-13 07:06:16 +0100

    Adding auth guard to the hono app

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 import { Hono } from 'hono'
+import { AuthMiddleware } from './infrastructure/http/hono/middlewares/jwt'
+import { HonoJwtService } from './infrastructure/security/HonoJwtService'
+
+const jwtService = new HonoJwtService(process.env.SECRET || "", 3600, Number(process.env.DAYS) || 7)
+const authMiddlewareInstance = new AuthMiddleware(jwtService)
+export const authGuard = authMiddlewareInstance.createMiddleware()
 
 const app = new Hono()
 
