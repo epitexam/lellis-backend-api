def35fc7b7496d1d21f05af6889d0c58377c3a13
Epitexam <202238868+epitexam@users.noreply.github.com>
2025-12-15 04:10:23 +0100

    Adding more handled errors

--- src/domain/auth/enums/TokenErrorType.ts
+++ src/domain/auth/enums/TokenErrorType.ts
@@ -9,7 +9,11 @@ export enum TokenErrorType {
     INVALID_PAYLOAD = "Invalid payload",
     INVALID_OR_EXPIRED_TOKEN = "The token provided is either invalid or expired",
     EXPIRED_TOKEN = "The token provided as expired.",
-    REFRESH_TOKEN_NOT_FOUND = "Refresh token not found in the database."
+    REFRESH_TOKEN_NOT_FOUND = "Refresh token not found in the database.",
+    ERROR_WHILE_GENERATING_ACCESS_TOKEN = "Error occured while generating access token.",
+    ERROR_WHILE_GENERATING_REFRESH_TOKEN = "Error occured while generating refresh token.",
+    MISSING_SECRET = "Missing secret"
+
 }
 
 /**
@@ -20,7 +24,10 @@ export const TokenErrorHttpStatus: Record<TokenErrorType, number> = {
     [TokenErrorType.INVALID_PAYLOAD]: HttpStatusCodes.BAD_REQUEST,
     [TokenErrorType.INVALID_OR_EXPIRED_TOKEN]: HttpStatusCodes.UNAUTHORIZED,
     [TokenErrorType.EXPIRED_TOKEN]: HttpStatusCodes.UNAUTHORIZED,
-    [TokenErrorType.REFRESH_TOKEN_NOT_FOUND]: HttpStatusCodes.NOT_FOUND
+    [TokenErrorType.REFRESH_TOKEN_NOT_FOUND]: HttpStatusCodes.NOT_FOUND,
+    [TokenErrorType.ERROR_WHILE_GENERATING_ACCESS_TOKEN]: HttpStatusCodes.INTERNAL_SERVER_ERROR,
+    [TokenErrorType.ERROR_WHILE_GENERATING_REFRESH_TOKEN]: HttpStatusCodes.INTERNAL_SERVER_ERROR,
+    [TokenErrorType.MISSING_SECRET]: HttpStatusCodes.INTERNAL_SERVER_ERROR
 };
 
 /**
