f5491734645c7251c1d20ca4ec994ebbbcb496d0
Epitexam <202238868+epitexam@users.noreply.github.com>
2025-12-16 21:16:51 +0100

    Revert "Creating auth routes files and using inside index.ts"
    This reverts commit 05d099152b2023bc4cf6ab332fb250c09ab4dc2d.

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,15 @@
-import { Hono } from 'hono';
-import { AuthMiddleware } from './infrastructure/http/hono/middlewares/jwt';
-import { HonoJwtService } from './infrastructure/security/HonoJwtService';
-import { authRoutes } from './infrastructure/http/hono/routes/auth';
-import { bootstrapDatabase } from './infrastructure/database/config/mysql';
+import { Hono } from 'hono'
+import { AuthMiddleware } from './infrastructure/http/hono/middlewares/jwt'
+import { HonoJwtService } from './infrastructure/security/HonoJwtService'
 
-const jwtService = new HonoJwtService(process.env.SECRET || "a_very_long_and_secure_secret_at_least_32_chars", 3600, Number(process.env.DAYS) || 7);
-const authMiddlewareInstance = new AuthMiddleware(jwtService);
-export const authGuard = authMiddlewareInstance.createMiddleware();
+const jwtService = new HonoJwtService(process.env.SECRET || "", 3600, Number(process.env.DAYS) || 7)
+const authMiddlewareInstance = new AuthMiddleware(jwtService)
+export const authGuard = authMiddlewareInstance.createMiddleware()
 
-const app = new Hono();
+const app = new Hono()
 
-(async () => {
-  // Space to lanche setup or config function
-  bootstrapDatabase()
-})();
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
 
-app.route('/auth', authRoutes);
-
-export default app;
+export default app
