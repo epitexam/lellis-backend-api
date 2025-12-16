e817d9c6c5b3ef7c353d1e76daf0d656fb037850
Epitexam <202238868+epitexam@users.noreply.github.com>
2025-12-16 21:16:51 +0100

    Revert "Using mysql with bun repository ( temporary )"
    This reverts commit 25ecc055ff466a861642a582d0a3f106709c369e.

--- src/infrastructure/services/user/CreateUserComposer.ts
+++ src/infrastructure/services/user/CreateUserComposer.ts
@@ -2,14 +2,14 @@ import { IIdProvider } from "../../../application/providers/IIdProvider";
 import { IPasswordHasher } from "../../../application/providers/IPasswordHasher";
 import { IUserRepository } from "../../../application/repositories/user/IUserRepository";
 import { CreateUserUseCase } from "../../../application/usecases/user/CreateUserUseCase";
-import { db } from "../../database/config/mysql";
-import { MySqlUserRepository } from "../../database/user/MySqlUserRepository";
+import { PrismaUserRepository } from "../../database/user/PrismaUserRepository";
 import { CreateUserController } from "../../http/hono/controllers/user/CreateUserController";
 import { BunIdProvider } from "../../provider/BunIdProvider";
 import { BunPasswordHasher } from "../../provider/BunPasswordHasher";
 
 export function CreateUserComposer() {
-    const repository: IUserRepository = new MySqlUserRepository(db)
+    const repository: IUserRepository = new PrismaUserRepository();
+
     const passwordHasher: IPasswordHasher = new BunPasswordHasher();
     const idProvider: IIdProvider = new BunIdProvider()
     
