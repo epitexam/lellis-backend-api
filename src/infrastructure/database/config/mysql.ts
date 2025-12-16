40151e1ba6f5e63e9fdf887ba3a757e26d665f8a
Epitexam <202238868+epitexam@users.noreply.github.com>
2025-12-16 21:16:51 +0100

    Revert "Increasing time between logs"
    This reverts commit bbab617cd37cdde7bc72a1db1121fd4dcba7b7bf.

--- src/infrastructure/database/config/mysql.ts
+++ src/infrastructure/database/config/mysql.ts
@@ -119,7 +119,7 @@ export async function bootstrapDatabase(): Promise<void> {
   try {
     console.log(`Connecting to database ${dbConfig.database} on ${dbConfig.host}...`);
     await setupDatabase();
-    monitorConnections(10000);
+    monitorConnections(2000);
 
     const handleShutdown = async () => {
       await closeDatabase();
