660b98b4d05aba6e2a9bc42f46a82b5362ba1d91
Epitexam <202238868+epitexam@users.noreply.github.com>
2025-12-16 21:16:51 +0100

    Revert "Adding hono refresh token route"
    This reverts commit 803bb30ae3fa47849c7ec976a04495c95abfa085.

--- src/infrastructure/http/hono/routes/auth.ts
+++ src/infrastructure/http/hono/routes/auth.ts
@@ -1,7 +1,6 @@
 import { Hono } from "hono";
 import { CreateUserComposer } from "../../../services/user/CreateUserComposer";
 import { LoginComposer } from "../../../services/auth/LoginComposer";
-import { RefreshTokenComposer } from "../../../services/auth/RefreshTokenComposer";
 
 export const authRoutes = new Hono()
 
@@ -13,14 +12,8 @@ authRoutes.post('/register', (c) => {
 
 authRoutes.post('/login', (c) => {
     const controller = LoginComposer();
-    const data = controller.handle(c);
-    return data
-})
-
-authRoutes.post('/refresh', (c) => {
-    const controller = RefreshTokenComposer()
-    const data = controller.handle(c);
-    return data
+    const udata = controller.handle(c);
+    return udata
 })
 
 authRoutes.get('/help', (c) => {
