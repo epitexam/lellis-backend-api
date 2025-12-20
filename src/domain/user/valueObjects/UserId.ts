2a0649ef9e0268cf6064cce85944bdfe8227aa35
Epitexam <202238868+epitexam@users.noreply.github.com>
2025-12-20 13:47:43 +0100

    Correcting User id value object class

--- src/domain/user/valueObjects/UserId.ts
+++ src/domain/user/valueObjects/UserId.ts
@@ -1,20 +1,15 @@
+import { BaseId } from "../../shared/valueObjects/BaseId";
 import { UserError, UserErrorType } from "../enums/UserErrorType";
 
-export class UserId {
-    private constructor(private readonly value: string) { }
+export class UserId extends BaseId<UserId> {
+    private constructor(value: string) {
+        super(value);
+    }
 
-    static create(id: string): UserId {
-        if (!id || typeof id !== "string" || !/^[0-9a-f]{8}-[0-9a-f]{4}-[7][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(id)) {
+    public static create(id: string): UserId {
+        if (!id || !BaseId.isValidUuid(id)) {
             throw new UserError(UserErrorType.INVALID_USER_ID);
         }
         return new UserId(id);
-    }
-
-    toString(): string {
-        return this.value;
-    }
-
-    equals(other: UserId): boolean {
-        return this.value === other.value;
     }
 }
